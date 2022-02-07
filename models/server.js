01176bea1cc34284849b1ca3a241c82e5b79eb77
Miguel Sánchez <migsandia@gmail.com>
2022-02-07 17:17:28 +0100

    Path del socket

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
 
@@ -25,6 +26,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+        // CORS
+        this.app.use( cors() );
     }
 
     configurarSockets() {
